ecfc4d349d9e934f4fbf634b214a5183d3431dbe
Cemal Hekim <98236326+cemalhekim@users.noreply.github.com>
2024-06-15 22:43:55 +0200

    Update main.bash

--- main.bash
+++ main.bash
@@ -1,3 +1,11 @@
+#in the beginning
+
 source /opt/ros/iron/setup.bash
 ros2 pkg create --build-type ament_python ur5e_navigation
 cd ur5e_navigation
+
+#in the end
+
+colcon build
+source install/setup.bash
+ros2 launch ur5e_navigation ur5e_navigation_launch.py

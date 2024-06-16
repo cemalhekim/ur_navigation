3bff1ae4aca907506be6e0707e059eced88724dc
Cemal Hekim <98236326+cemalhekim@users.noreply.github.com>
2024-06-16 14:15:17 +0200

    Create setup.py

---
+++ basic_navigation_node/W Requirements/setup.py
@@ -0,0 +1,32 @@
+# setup.py
+
+from setuptools import setup
+from glob import glob
+import os
+
+package_name = 'ur5e_navigation'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='your_name',
+    maintainer_email='cemalhekim@icloud.com',
+    description='UR5e navigation using ROS 2 Iron and MoveIt',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'lifecycle_navigation_node = ur5e_navigation.lifecycle_navigation_node:main',
+            'reach_pose_action_server = ur5e_navigation.reach_pose_action:main',
+        ],
+    },
+)
